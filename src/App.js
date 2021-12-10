d4b64abb636a8c1225476119b11598fb07bb1a60
DaniD98 <danielle1998dennis@gmail.com>
2021-12-10 12:56:08 +0000

    1 file modified

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import MedicalTreatment from "./MedicalTreatment.js";
 export default function Apps() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
+      <h1>Medical Treatment!</h1>
       <MedicalTreatment treatId = "100" treatCourseId = "67675" type = "Breast Cancer" category = "Cancer Treatment" name = "Simone Dod" startDate = '12/12/2021'/>
       </div>
   );
